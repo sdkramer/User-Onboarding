3f74963864a7810ab640ca62b02605de61ed1f28
Sean Kramer <seandominickramer@gmail.com>
2020-05-12 21:47:56 -0400

    Add error warning for invalid email

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,99 +1,101 @@
-import React, { useState, useEffect } from 'react'
-import Form from './Form'
-import UserList from './UserList'
-import axios from 'axios'
-import * as yup from 'yup'
+import React, { useState, useEffect } from "react";
+import Form from "./Form";
+import UserList from "./UserList";
+import axios from "axios";
+import * as yup from "yup";
 
 const formSchema = yup.object().shape({
-  username: yup.string().required('Please enter name'),
-  email: yup.string().email('Must be a valid email').required('Please enter your email address'),
-  password: yup.string().min(7, 'Password must include at least 7 characters').required('Please enter your password'),
-  terms: yup.boolean().oneOf([true], 'You must accept the terms to continue'),
-
+  username: yup.string().required("Please enter name"),
+  email: yup
+    .string()
+    .email("Must be a valid email")
+    .required("Please enter your email address"),
+  password: yup
+    .string()
+    .min(7, "Password must include at least 7 characters")
+    .required("Please enter your password"),
+  terms: yup.boolean().oneOf([true], "You must accept the terms to continue"),
 });
 
 function App() {
-
   const initialFormValues = {
-    username: '',
-    email: '',
-    password: '',
-    terms: '',
-  }
-  
-  const initialUserList = {
-    username: 'Tim',
-    email: 't@tim.com',
-    password: 'af45g*o',
+    username: "",
+    email: "",
+    password: "",
     terms: false,
-  }
+  };
 
-  const [users, setUsers] = useState(initialUserList)
- 
+  const initialUserList = {
+    username: "Tim",
+    email: "t@tim.com",
+    password: "af45g*o",
+    terms: false,
+  };
 
-  const [formValues, setFormValues] = useState(initialFormValues)
-  console.log(formValues)
+  const [users, setUsers] = useState(initialUserList);
 
-const [errorState, setErrorState] = useState({
-  username: '',
-  email: '',
-  password: '',
-  terms: '',
-});
+  const [formValues, setFormValues] = useState(initialFormValues);
+  // console.log(formValues)
 
-const validate = event => {
-  yup.reach(formSchema, event.target.name)
-  .validate(event.target.value)
-  .then(valid => {
-    setErrorState({...errorState, [event.target.name]: ''})
-  })
-  .catch(err => {
-    console.log(err.errors)
-    setErrorState({
-      ...errorState, [event.target.name]: err.errors[0]
-    })
-  })
-}
+  const [errorState, setErrorState] = useState({
+    username: "",
+    email: "",
+    password: "",
+    terms: "",
+  });
 
-const onSubmit = (event) => {
-event.preventDefault();
-console.log('submitted');
-axios.post('https://reqres.in/api/users', formValues)
-.then(response => console.log(response.data))
-.catch(err => console.log(err))
-}
+  const validate = (event) => {
+    yup
+      .reach(formSchema, event.target.name)
+      .validate(event.target.value)
+      .then((valid) => {
+        setErrorState({ ...errorState, [event.target.name]: "" });
+      })
+      .catch((err) => {
+        console.log(err.errors);
+        setErrorState({
+          ...errorState,
+          [event.target.name]: err.errors[0],
+        });
+      });
+  };
 
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log("submitted");
+    axios
+      .post("https://reqres.in/api/users", formValues)
+      .then((response) => console.log(response.data))
+      .catch((err) => console.log(err));
+  };
 
-
-
-
-  const onInputChange = event => {
+  const onInputChange = (event) => {
     event.persist();
     validate(event);
-    const name = event.target.name
-    const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
-setFormValues({...formValues,
-[name]: value})
-  }
-
+    const name = event.target.name;
+    const value =
+      event.target.type === "checkbox"
+        ? event.target.checked
+        : event.target.value;
+    setFormValues({ ...formValues, [name]: value });
+  };
 
   return (
     <div className="container-div">
-      <header><h1>User Application</h1></header>
-      
-      <Form 
+      <header>
+        <h1>User Application</h1>
+      </header>
+
+      <Form
         values={formValues}
         onInputChange={onInputChange}
         onSubmit={onSubmit}
+        errorState={errorState}
       />
 
-      <UserList 
-        users={users}
-      />
- 
+      <UserList users={users} />
     </div>
   );
 }
 
 export default App;
-
--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Form(props) {
-  const { onInputChange, values, onSubmit } = props;
+  const { onInputChange, values, onSubmit, errorState } = props;
 
   return (
     <form className="form-container" onSubmit = {onSubmit}>
@@ -15,9 +15,14 @@ function Form(props) {
           values={values.username}
           onChange={onInputChange}
         />
+        {errorState.username.length > 0 ? (<p classname='error'>{errorState.username}</p>): null}
       </p>
       <p className="clearfix">
-        <label htmlFor='email'>Email</label>
+        <label htmlFor='email'>Email
+        {errorState.email.length > 0 ? (
+          <p className="error">{errorState.email}</p>
+        ) : null}
+        </label>
         <input
           className="input-field"
           name="email"
@@ -25,7 +30,7 @@ function Form(props) {
           placeholder="Email"
           values={values.email}
           onChange={onInputChange}
-        />
+        />   
       </p>
       <p className="clearfix">
         <label htmlFor='password'>Password</label>
@@ -39,7 +44,11 @@ function Form(props) {
         />
       </p>
       <p className="clearfix check">
-        <label htmlFor='terms'>Terms of Service</label>
+        <label htmlFor='terms'>Terms of Service
+        {errorState.terms.length > 0 ? (
+          <p className="error">{errorState.terms}</p>
+        ) : null}
+        </label>
         <input
           className="input-field"
           name="terms"
